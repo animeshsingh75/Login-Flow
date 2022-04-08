dab0a4de4681188e5c8139700cc36fb28cc418c8
Animesh Singh <animeshsingh75@gmail.com>
2022-04-08 15:53:21 +0530

    Login Flow

---
+++ app/src/main/java/com/example/loginflow/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.loginflow
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginflow.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            binding.animationView.cancelAnimation()
+            val intent = Intent(this, PhoneActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        binding.animationView.resumeAnimation()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        binding.animationView.pauseAnimation()
+        super.onPause()
+    }
+}
---
+++ app/src/main/java/com/example/loginflow/PhoneActivity.kt
@@ -0,0 +1,27 @@
+package com.example.loginflow
+
+import android.R.attr.phoneNumber
+import android.content.Intent
+import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.example.loginflow.databinding.ActivityPhoneBinding
+
+class PhoneActivity : AppCompatActivity() {
+    lateinit var binding: ActivityPhoneBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPhoneBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.backArrowImage.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        binding.phoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+        binding.otpButton.setOnClickListener {
+            val intent = Intent(this, OtpActivity::class.java)
+            intent.putExtra("phone_number",binding.phoneNumber.text.toString())
+            startActivity(intent)
+        }
+    }
+}
